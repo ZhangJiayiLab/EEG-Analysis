d45fa2a76312bd0c646358e6e367e9293f9c4b50
zane muir <zanemuir1995@gmail.com>
2018-09-21 22:09:29 +0800

    probe layout update

--- python/probelayout.py
+++ python/probelayout.py
@@ -3,17 +3,18 @@ Probe layout information
 
 author: Yizhan Miao
 email: yzmiao@protonmail.com
-last update: Sept 20 2018
+last update: Sept 21 2018
 """
 
+import os
 import pandas as pd
 
 class ProbeLayout(object):
-    def __init__(self, layout_dic="dic.csv"):
-        self.layout = pd.read_csv(layout_dic)
-        
+    def __init__(self, datadir, layout_dic=):
+        self.layout = pd.read_csv(os.path.join(datadir, layout_dic))
+
     def findchannel(self, area):
         return self.layout[self.layout.position == area].channel.values
-    
+
     def findarea(self, channel):
         return self.layout[self.layout.channel == channel].position.values
